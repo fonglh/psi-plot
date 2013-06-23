23a30591da4a905d7bd2022de9d072acc98399ef
flh <flh@server.com>
2013-06-24 01:09:48 +0800

    include PSI 24 hour readings as columnrange chart. add highcharts-more.js file which is necessary for columnrange charts.

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
 		<title>Singapore PSI Readings</title>
 		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js"></script>
 		<script src="highstock.js"></script>
+		<script src="highcharts-more.js"></script>
 
 		<?php
 
@@ -15,37 +16,87 @@
 			$dbpass = 'aNqL6bA5';
 
 			$db = new MongoClient("mongodb://$dbuser:$dbpass@$dbhost/$dbname");
-			$c_readings = $db->selectCollection( $dbname, "psi_readings");
+			$c_readings = $db -> selectCollection( $dbname, "psi_readings" );
 
+			// get 3 hour PSI readings
 			$cursor = $c_readings->find();
 			$cursor->sort( array('timestamp' => 1) );
 
-			$data_str = "[";
+			$psi_3hr_str = "[";
 
 			while ( $psi_reading = $cursor -> getNext() )
 			{
 				// javascript timestamps are in milliseconds so the values need to be multipled by 1000
-				$data_str .= "[" . $psi_reading[ 'timestamp' ] . "000, " . $psi_reading[ 'psi' ] . "], ";
+				$psi_3hr_str .= "[" . $psi_reading[ 'timestamp' ] . "000, " . $psi_reading[ 'psi' ] . "], ";
 			}
 
 			//replace final ", " with "]"
-			$data_str = substr( $data_str, 0, strlen( $data_str ) - 2 );
-			$data_str .= "]"; 
+			$psi_3hr_str = substr( $psi_3hr_str, 0, strlen( $psi_3hr_str ) - 2 );
+			$psi_3hr_str .= "]"; 
+
+			//get 24 hour PSI readings
+			$c_readings = $db -> selectCollection( $dbname, "psi_24hr_pm25" );
+			$aggregate_ops = array(
+								array(
+									'$group' => array(
+												"_id" => '$timestamp',
+												"min" => array('$min'=>'$psi_24'),
+												"max" => array('$max'=>'$psi_24')
+												)
+									),
+								array(
+									'$project' => array(
+													"timestamp" => '$_id',
+													'_id' => 0,
+													"min" => 1,
+													"max" => 1,
+													)
+									),
+								array(
+									'$sort' => array(
+													'timestamp' => 1
+													)
+									)
+								);
+													
+			$psi_24hr_data = $c_readings -> aggregate( $aggregate_ops );
+			$psi_24hr_data = $psi_24hr_data['result'];
+
+			$psi_24hr_str = "[";
+
+			foreach ( $psi_24hr_data as &$psi_reading )
+			{
+
+				// javascript timestamps are in milliseconds so the values need to be multipled by 1000
+				$psi_24hr_str .= "[" . $psi_reading[ 'timestamp' ] . "000, " . $psi_reading[ 'min' ] . ", " . $psi_reading[ 'max' ] . "], ";
+			}
+
+			//replace final ", " with "]"
+			$psi_24hr_str = substr( $psi_24hr_str, 0, strlen( $psi_24hr_str ) - 2 );
+			$psi_24hr_str .= "]"; 
+
 		?>
 		<script>
 		var chart1;		//globally available
 		$(function () {
 			chart1 = new Highcharts.StockChart({
 				chart: {
-					renderTo: 'container'
+					renderTo: 'container',
 				},
 				rangeSelector: {
 					selected: 1
 				},
-				series: [{
+				series: [
+				{
 					name: 'PSI Reading',
-					data: <?php echo $data_str; ?>
-				}]
+					data: <?php echo $psi_3hr_str; ?>
+				},
+				{
+					name: 'PSI 24 hour Reading',
+					type: 'columnrange',
+					data: <?php echo $psi_24hr_str; ?>
+				}
+				]
 			});
 
 			Highcharts.setOptions({
