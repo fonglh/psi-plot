9324ab8d27c198202631bf4c4568c8afa550c95c
flh <flh@server.com>
2013-06-24 23:41:52 +0800

    add PM2.5 graph to index.php. Customize zoom buttons, labels and plotbands.

--- index.php
+++ index.php
@@ -63,7 +63,7 @@
 								);
 													
 			$psi_24hr_data = $c_readings -> aggregate( $aggregate_ops );
-			$psi_24hr_data = $psi_24hr_data['result'];
+			$psi_24hr_data = $psi_24hr_data[ 'result' ];
 
 			$psi_24hr_str = "[";
 
@@ -78,17 +78,68 @@
 			$psi_24hr_str = substr( $psi_24hr_str, 0, strlen( $psi_24hr_str ) - 2 );
 			$psi_24hr_str .= "]"; 
 
+			//get PM2.5 readings
+			$aggregate_ops = array(
+								array(
+									'$group' => array(
+												"_id" => '$timestamp',
+												"min" => array('$min'=>'$pm25'),
+												"max" => array('$max'=>'$pm25')
+												)
+									),
+								array(
+									'$project' => array(
+													"timestamp" => '$_id',
+													'_id' => 0,
+													"min" => 1,
+													"max" => 1,
+													)
+									),
+								array(
+									'$sort' => array(
+													'timestamp' => 1
+													)
+									)
+								);
+			$pm25_data = $c_readings -> aggregate( $aggregate_ops );
+			$pm25_data = $pm25_data[ 'result' ];
+
+			$pm25_str = "[";
+
+			foreach ( $pm25_data as &$psi_reading )
+			{
+
+				// javascript timestamps are in milliseconds so the values need to be multipled by 1000
+				$pm25_str .= "[" . $psi_reading[ 'timestamp' ] . "000, " . $psi_reading[ 'min' ] . ", " . $psi_reading[ 'max' ] . "], ";
+			}
+
+			//replace final ", " with "]"
+			$pm25_str = substr( $pm25_str, 0, strlen( $pm25_str ) - 2 );
+			$pm25_str .= "]"; 
+
 		?>
 		<script>
 		var chart1;		//globally available
+		var chart2;
 		$(function () {
+			Highcharts.setOptions({
+				global: {
+					useUTC: false
+				}
+			});
+
 			chart1 = new Highcharts.StockChart({
 				chart: {
-					renderTo: 'container',
+					renderTo: 'psi_container',
 				},
 				rangeSelector: {
 					selected: 1
 				},
+
+				title: {
+					text: 'PSI Readings'
+				},
+
 				legend: {
 					enabled: true,
 					align: 'right',
@@ -98,6 +149,32 @@
 					layout: 'vertical',
 					itemMarginBottom: 10,
 				},
+
+				rangeSelector: {
+					buttons: [
+					{
+						type: 'day',
+						count: 7,
+						text: '7d'
+					}, 
+					{
+						type: 'day',
+						count: 3,
+						text: '3d'
+					},
+					{
+						type: 'day',
+						count: 1,
+						text: '1d'
+					},
+					{
+						type: 'all',
+						text: 'All'
+					}
+					],
+					selected: 2
+				},
+
 				series: [
 				{
 					name: 'PSI 3-hr Reading',
@@ -111,11 +188,70 @@
 				]
 			});
 
-			Highcharts.setOptions({
-				global: {
-					useUTC: false
-				}
+			chart2 = new Highcharts.StockChart({
+				chart: {
+					renderTo: 'pm25_container',
+				},
+				rangeSelector: {
+					selected: 1
+				},
+
+				title: {
+					text: '24-hr PM2.5 Concentration (ug/m^3)'
+				},
+
+				yAxis: {
+					title: {
+						text: 'Concentration'
+					},
+					plotBands: [{
+						from: 51,
+						to: 100,
+						color: 'rgba(145, 255, 0, 0.5)',
+						label: { text: 'Moderate' }
+					},
+					{
+						from: 201,
+						to: 300,
+						color: 'rgba(255, 145, 0, 0.8)',
+						label: { text: 'Very unhealthy' }
+					}]
+				},
+
+				rangeSelector: {
+					buttons: [
+					{
+						type: 'day',
+						count: 7,
+						text: '7d'
+					}, 
+					{
+						type: 'day',
+						count: 3,
+						text: '3d'
+					},
+					{
+						type: 'day',
+						count: 1,
+						text: '1d'
+					},
+					{
+						type: 'all',
+						text: 'All'
+					}
+					],
+					selected: 2
+				},
+
+				series: [
+				{
+					name: '24-hr PM2.5 Concentration',
+					type: 'columnrange',
+					data: <?php echo $pm25_str; ?>
+				},
+				]
 			});
+
 
 		});
 
@@ -124,7 +260,8 @@
 	</head>
 
 	<body>
-		<div id="container" style="width:80%; height:400px; margin-left:auto; margin-right:auto;"></div>
+		<div id="psi_container" style="width:80%; height:400px; margin-left:auto; margin-right:auto;"></div>
+		<div id="pm25_container" style="width:80%; height:400px; margin-left:auto; margin-right:auto;"></div>
 	</body>
 	<?php include_once("analyticstracking.php") ?>
 </html>
