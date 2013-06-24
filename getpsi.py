5769cd75c3bb710ae249b2ef7a7e4a40b8e6f015
flh <flh@server.com>
2013-06-24 23:00:08 +0800

    changed method of detecting when a value is invalid. this can happen when the script is run after midnight, but before NEA has updated its website for the next day.

--- getpsi.py
+++ getpsi.py
@@ -14,7 +14,8 @@ class GMT8(tzinfo):
 	def tzname(self,dt):
 		return "GMT +8"
 
-currdt = datetime.now(tz=GMT8())
+currdt = datetime.now(tz=GMT8())	# this datetime var will be used for data insertion
+dtnow = datetime.now(tz=GMT8())		# time at which the script is run
 
 #get database
 client = MongoClient()
@@ -24,17 +25,6 @@ collection = db.psi_readings
 
 f = urllib.urlopen("http://app2.nea.gov.sg/anti-pollution-radiation-protection/air-pollution/psi/past-24-psi-readings")
 psihtml = f.read()
-
-# get current day to check if the site has been updated just past midnight
-start_day = psihtml.find('<h1 id="psi24">')
-psihtml = psihtml[ start_day: ]
-end_day = psihtml.find('</h1>')
-day = re.findall(r'24-hr PSI Readings on ([0-9]+) [A-Za-z]{3} \d{4}', psihtml[:end_day])
-day = int(day[0])
-
-# quit as reading is not in yet
-if day != currdt.day:
-	exit(0)
 
 #find start of PSI reading table
 start_psi = psihtml.find("<h1>3-hr PSI Readings from 12AM to 11.59PM on")
@@ -58,8 +48,10 @@ hr = 0
 for reading in psi_readings:
 	if reading.isdigit():
 		currdt = currdt.replace(hour=hr, minute=0, second=0, microsecond=0, tzinfo=GMT8())
-		ts = int(time.mktime(currdt.timetuple()))
-		entry = { "timestamp": ts, "psi": reading }
-		collection.update( { "timestamp": ts}, entry, upsert=True )
-		print currdt, ts, int(reading)
+		#only insert data if the timestamp is before the time now, there CAN'T be future data!!!
+		if currdt < dtnow:		
+			ts = int(time.mktime(currdt.timetuple()))
+			entry = { "timestamp": ts, "psi": reading }
+			collection.update( { "timestamp": ts}, entry, upsert=True )
+			print currdt, ts, int(reading)
 	hr += 1
