a170bd889ad4fb97d16952b790b0cd0ee5168a14
flh <flh@server.com>
2014-10-06 17:07:06 +0800

    Add mongo db init tests. Add tests to check if NEA website should be polled. Use the mongomock module to mock the data needed.

--- psi_tests.py
+++ psi_tests.py
@@ -1,6 +1,41 @@
 import unittest
+import mongomock
+from datetime import datetime, tzinfo, timedelta
+import pymongo
 import getpsi
 
+class GMT8(tzinfo):
+	def utcoffset(self, dt):
+		return timedelta(hours=8) + self.dst(dt)
+	def dst(self, dt):
+		return timedelta(0)
+	def tzname(self,dt):
+		return "GMT +8"
+
+class DatabaseTests(unittest.TestCase):
+	def test_db_init_psi_readings(self):
+		collection = getpsi.db_init('psi_db', 'psi_readings')
+		assert isinstance(collection, pymongo.collection.Collection)
+
+	def test_db_init_fail(self):
+		collection = getpsi.db_init('non_existent', 'non_existent')
+		self.assertIsNone(collection)
+
 class Psi3Hour(unittest.TestCase):
-	def test_poll_site(self):
-		assert True
+	def test_should_poll_nea_yes(self):
+		collection = mongomock.Connection().db.collection
+		data_time = datetime.now(tz=GMT8()) - timedelta(hours=2)
+		# latest timestamp more than 1 hour ago
+		collection.insert(dict(timestamp=data_time))
+
+		result = getpsi.should_poll_nea(collection)
+		self.assertIs(result, True)
+
+	def test_should_poll_nea_no(self):
+		collection = mongomock.Connection().db.collection
+		data_time = datetime.now(tz=GMT8()) - timedelta(minutes=2)
+		# latest timestamp more than 1 hour ago
+		collection.insert(dict(timestamp=data_time))
+
+		result = getpsi.should_poll_nea(collection)
+		self.assertIs(result, False)
